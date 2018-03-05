3f4f2f1b03a05331b8172a87fcb389e71e5b5389
walkerthon2 <awalkerr@hotmail.com>
2018-03-06 00:35:53 +1100

    Actual Analysis Here

--- New_Analysis.R
+++ New_Analysis.R
@@ -1,1 +1,74 @@
 #Test
+require('tidyverse')
+require('R.matlab')
+
+pNum <- 6 #Number of participants to analyse
+cNum <- 2 #Number of conditions to Analyse
+
+for (cond in 1:cNum){
+  for(file in 1:pNum) {
+    fileName <- paste(c("C",cond, "_S",file, '_ET1_processed_final.mat'), collapse = '') #Read in data file
+    
+    setwd('../Final Data (Stage 1)')
+    tempData <- readMat(fileName) 
+    tempData1 <- tempData$finalData #Create data frame from part 1
+    tempData1 <- data.frame(tempData1)
+    
+    setwd('../Final Data (Stage 2)')
+    tempData <- readMat(fileName) #Create data frame from part 2
+    tempData2 <- tempData$finalData #Create data frame from part 1
+    tempData2 <- data.frame(tempData2)
+    tempData2$X2 <- tempData2$X2 + 18
+    tempData2$X1 <- tempData2$X1 + 288
+    
+    subData = merge(tempData1, tempData2, all = TRUE) #Merge the two parts together together
+    
+    subData <- mutate(subData, Subject = file) #Add the subject number to the data frame
+    subData <- mutate(subData, Condition = cond) #Add the condition to the data frame
+    
+    if (file > 1 | cond > 1) { #Add this participants data to the large data frame
+      finalData <- merge(finalData, subData, all = TRUE) 
+    } else {
+      finalData <- subData
+      
+    }
+    
+  }
+}
+
+colnames(finalData) <- c('Trial', 'Block', 'LeftCue', 'RightCue', 'PredLoc', 'OptResponse', 
+                         'Optimal', 'Suboptimal', 'Reward', 'RandVar','totalPoints', 
+                         'LocCounter', 'RT', 'nothing?', 
+                         'FixLeft', 'TimeLeft', 'FixRight', 'TimeRight', 
+                         'FixPred', 'TimePred', 'FixNP', 'TimeNP', 'PropPred', 'PropNP', 'TimeCross',
+                         'Subject', 'Condition') #Name the columns
+
+finalData <-  mutate(finalData, Section = case_when(Block <= 18 ~ 1,
+                                          Block > 18 ~ 2)) #Create sections
+
+finalData <- mutate(finalData, ShortBlock = case_when(Block == 1 | Block == 2 ~ 1,
+                                                      Block == 3 | Block == 4 ~ 2,
+                                                      Block == 5 | Block == 6 ~ 3,
+                                                      Block == 7 | Block == 8 ~ 4,
+                                                      Block == 9 | Block == 10 ~ 5,
+                                                      Block == 11 | Block == 12 ~ 6,
+                                                      Block == 13 | Block == 14 ~ 7,
+                                                      Block == 15 | Block == 16 ~ 8,
+                                                      Block == 17 | Block == 18 ~ 9,
+                                                      Block == 19 | Block == 20 ~ 10,
+                                                      Block == 21 | Block == 22 ~ 11,
+                                                      Block == 23 | Block == 24 ~ 12,
+                                                      Block == 25 | Block == 26 ~ 13))
+
+finalData <- as_tibble(finalData)
+
+finalData <-filter(finalData, !(Condition == 1 & Subject == 1)) #Filter bad participants (DON'T FORGET TO REMOVE THESE)
+finalData <-filter(finalData, !(Condition == 1 & Subject == 5))
+
+testBlock <- arrange(finalData, Condition, Subject, Trial) %>% 
+  group_by(Condition, ShortBlock) %>% 
+  summarise(mean(Optimal))#sort by subject then collapse
+
+#Create some pretty graphs
+
+setwd('../New_Analysis')
